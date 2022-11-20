a34a5ba06a9e4387c51493a36c79923345f86b61
Rafat <rafatansari12feb1999@gmail.com>
2022-11-20 15:45:47 +0530

    feat:replace routing using state with react-router

---
+++ starter/src/SearchPage.js
@@ -0,0 +1,22 @@
+import {Link} from "react-router-dom" 
+
+const SearchPage = () => {
+    return (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link to={"/"} className="close-search">Close</Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title, author, or ISBN"
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      );
+}
+
+export default SearchPage;
